5e3b8832511c4d2bdd2bee56289739a9aa759657
Zenza235 <paoloatienza235@gmail.com>
2025-12-15 01:30:56 -0500

    add: "About", "Home" pages; tailwindcss setup

---
+++ src/pages/About.tsx
@@ -0,0 +1,12 @@
+import Header from "../components/Header";
+
+function About() {
+  return (
+    <>
+      <Header />
+      <h1 className="text-3xl">About</h1>
+    </>
+  );
+}
+
+export default About;
